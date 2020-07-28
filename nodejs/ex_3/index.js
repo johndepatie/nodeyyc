c3c7619083b7038bd8b512bf6a221f556cbea9f5
johndepatie <john.depatie1@gmail.com>
2020-07-27 19:49:21 -0600

    Added NodeJS exercise 3

---
+++ nodejs/ex_3/index.js
@@ -0,0 +1,7 @@
+const Logger = require('logplease');
+const logger = Logger.create('utils');
+logger.debug(`Hello Node.js`);
+logger.log(`Hello World`); // alias for debug()
+logger.info(`Node.js is great!!`);
+logger.warn(`Warning, Warning, I think we have a Warning`);
+logger.error(`Mayday, Mayday, we have an errro, repeat... we have an error`);
