282acc39f4a03f2070b77ce91a797fbc2c859d6f
johndepatie <john.depatie1@gmail.com>
2020-07-29 18:31:10 -0600

    Added Nodejs Ex_6

--- nodejs/ex_6/index.js
+++ nodejs/ex_6/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const greet = (first_name, last_name, message) => {
+    console.log ("Hi " + first_name + " " + last_name);
+    message(first_name);
+}
+
+
+const message = (name) => {
+    console.log("Nice to see you again " + name +"!");
+}
+
+greet("Kayla", "Depatie", message);
